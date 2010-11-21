4c02d0bb2d454e3b7ded2afa635991adf42a7244
James Polley <jamezpolley@gmail.com>
2010-11-21 20:22:54 +1100

    more third_party libraries. config.py imports from the zipfile. mkzip creates the zipfile, ignoring crud we don't care about

--- config.py
+++ config.py
@@ -2,5 +2,7 @@
 import sys
 
 # Add our extra modules to sys.path
-sys.path.append('third_party/aeoid')
-sys.path.append('third_party/python-datetime-tz')
+sys.path.insert(0, 'third_party.zip/aeoid')
+sys.path.insert(0, 'third_party.zip/python-dateutil-1.5')
+sys.path.insert(0, 'third_party.zip/python-datetime-tz')
+sys.path.insert(0, 'third_party.zip/pytz/src')
