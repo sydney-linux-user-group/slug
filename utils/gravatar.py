ed73035392416535f5c73b15e160e93550e08a06
Tim Ansell <mithro@mithis.com>
2010-11-21 14:09:03 +1100

    Adding a gravatar generator.

---
+++ utils/gravatar.py
@@ -0,0 +1,12 @@
+# Based on example code from:
+#   http://en.gravatar.com/site/implement/images/python/
+
+import urllib, hashlib
+
+def gravatar(email, size=40):
+  default = "http://www.example.com/default.jpg"
+
+  # construct the url
+  gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
+  gravatar_url += urllib.urlencode({'d':default, 's':str(size)})
+  return gravatar_url
