5414ab8b76a1305abf4b929b97e6063d421eaa23
Tim Ansell <mithro@mithis.com>
2011-04-24 13:14:53 +1000

    Slight fixes to init_third_party

--- init_third_party.sh
+++ init_third_party.sh
@@ -8,6 +8,9 @@ cd third_party
 curl -O http://labix.org/download/python-dateutil/python-dateutil-1.5.tar.gz
 tar -zxvf python-dateutil-1.5.tar.gz
 
+rm -rf pytz
 bzr branch lp:pytz
 
 svn checkout http://python-datetime-tz.googlecode.com/svn/trunk/ python-datetime-tz
+
+./mkzip
