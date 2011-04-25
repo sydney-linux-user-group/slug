ab14d2eb7895d133c94c0b2fe7491b45fc2b8db0
James Polley <jamezpolley@gmail.com>
2011-04-25 17:21:35 +1000

    tabs -> 4 spaces cleanup. Add vobject

--- config.py
+++ config.py
@@ -2,18 +2,18 @@
 import sys
 
 def sys_path_insert(module):
-	if module not in sys.path:
-		sys.path.insert(0, module)
+    if module not in sys.path:
+        sys.path.insert(0, module)
 
 def setup():
-	# Add our extra modules to sys.path
-	sys_path_insert('third_party/aeoid')
-	sys_path_insert('third_party.zip/python-dateutil-1.5')
-	sys_path_insert('third_party.zip/python-datetime-tz')
-	sys_path_insert('third_party.zip/icalendar-2.1/src')
-	sys_path_insert('third_party.zip/Markdown-2.0.3')
+    # Add our extra modules to sys.path
+    sys_path_insert('third_party/aeoid')
+    sys_path_insert('third_party.zip/python-dateutil-1.5')
+    sys_path_insert('third_party.zip/python-datetime-tz')
+    sys_path_insert('third_party.zip/vobject')
+    sys_path_insert('third_party.zip/Markdown-2.0.3')
 
-	import os
-	os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-	from google.appengine.dist import use_library
-	use_library('django', '1.2')
+    import os
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+    from google.appengine.dist import use_library
+    use_library('django', '1.2')
