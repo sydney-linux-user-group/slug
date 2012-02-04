0423580da0b46d006a3dd33ea93701346d360902
Tim 'mithro' Ansell <mithro@mithis.com>
2012-02-04 13:32:01 +1100

    usergroup.urls not slug.urls

--- settings.py
+++ settings.py
@@ -103,7 +103,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'slug.urls'
+ROOT_URLCONF = 'usergroup.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
