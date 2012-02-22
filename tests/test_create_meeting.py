84e49fe3c040aeaf4500d779d8c94b29623661d9
James Polley <jamezpolley@gmail.com>
2012-02-22 20:41:31 +1100

    Add basic tests for creating meetings

--- tests/test_create_meeting.py
+++ tests/test_create_meeting.py
@@ -4,18 +4,33 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 import time
+import unittest2 as unittest
 
-browser = webdriver.Firefox() # Get local session of firefox
-browser.get("http://localhost:8000")
-assert "Sydney Linux User Group" in browser.title
-login_link = browser.find_element_by_xpath
-login_link = browser.find_element_by_class_name("openid_login")
-login_link.click()
+class TestBasicSeleniumality(unittest.TestCase):
 
-browser.switch_to_window("login")
-browser.find_element_by_id("id_username").send_keys("admin")
-browser.find_element_by_id("id_password").send_keys("password")
-browser.find_element_by_id("submit_login").click()
+    def testBasics(self):
+        browser = webdriver.Firefox() # Get local session of firefox
+        browser.get("http://localhost:8000")
+        self.assertIn("Sydney Linux User Group", browser.title)
 
-browser.switch_to_window("parent")
-browser.close()
+    def tearDown(self):
+        self.browser.close()
+
+class TestLogin(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox() # Get local session of firefox
+        self.browser.get("http://localhost:8000")
+
+    def testLogin(self):
+        login_link = self.browser.find_element_by_xpath
+        login_link = self.browser.find_element_by_class_name("openid_login")
+        login_link.click()
+        self.browser.switch_to_window("login")
+        self.browser.find_element_by_id("id_username").send_keys("admin")
+        self.browser.find_element_by_id("id_password").send_keys("password")
+        self.browser.find_element_by_id("submit_login").click()
+        self.browser.switch_to_window("parent")
+
+    def tearDown(self):
+        self.browser.close()
