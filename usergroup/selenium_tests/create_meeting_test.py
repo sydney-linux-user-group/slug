cb42a3bcbf2a5da2d6766af73278e06482ec8d8f
James Polley <jamezpolley@gmail.com>
2012-02-22 23:01:04 +1100

    Current status of testing

--- usergroup/selenium_tests/create_meeting_test.py
+++ usergroup/selenium_tests/create_meeting_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from selenium import webdriver
+from selenium.selenium import selenium
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 import time
@@ -15,7 +16,7 @@ class SeleniumBasicsTestCase(LiveServerTestCase):
     def testBasics(self):
         self.browser = webdriver.Firefox() # Get local session of firefox
         self.browser.get("%s" % self.live_server_url)
-        self.assertIn("Sydney Linux User Group", browser.title)
+        self.assertIn("Sydney Linux User Group", self.browser.title)
 
     def tearDown(self):
         LiveServerTestCase.tearDown(self)
@@ -30,15 +31,23 @@ class TestLogin(LiveServerTestCase):
         self.browser.get("%s" % self.live_server_url)
 
     def testLogin(self):
-        login_link = self.browser.find_element_by_xpath
-        login_link = self.browser.find_element_by_class_name("openid_login")
+        self.assertEqual(1, len(self.browser.window_handles))
+        main_window_handle = self.browser.window_handles[0]
+        login_link = self.browser.find_element_by_id("login_link")
         login_link.click()
         self.browser.switch_to_window("login")
-        self.browser.find_element_by_id("id_username").send_keys("admin")
-        self.browser.find_element_by_id("id_password").send_keys("password")
+        self.browser.find_element_by_name("username").send_keys("admin")
+        self.browser.find_element_by_id("id_password").send_keys("admin")
         self.browser.find_element_by_id("submit_login").click()
-        self.browser.switch_to_window("parent")
+        time.sleep(30)
+        self.browser.switch_to_window(main_window_handle)
+        self.assertEqual(1, len(self.browser.window_handles))
+        self.assertIn("Sydney Linux User Group", self.browser.title)
+        logout_link = self.browser.find_element_by_id("logout_link")
+        logout_link.click()
+
 
     def tearDown(self):
-        LiveServerTestCase.tearDown(self)
-        self.browser.quit()
+        #LiveServerTestCase.tearDown(self)
+        #self.browser.quit()
+        pass
