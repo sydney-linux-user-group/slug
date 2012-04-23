04d243bf796f7cb4636cdc7a354501f9945f300f
James Polley <jpolley@atlassian.com>
2012-04-23 21:34:21 +1000

    Our tests are date-dependent - they only work prior to the 25th of March.
    To get around this, we monkey-patch the get_date function and have it
    lie about today's date.

--- usergroup/tests.py
+++ usergroup/tests.py
@@ -4,7 +4,7 @@
 # vim: set ts=4 sw=4 et sts=4 ai:
 
 import cStringIO as StringIO
-
+import datetime
 import os
 import re
 import socket
@@ -19,22 +19,30 @@ from django_testing_fixes import fix_fixtures
 from django_testing_fixes import fix_exceptions
 
 from usergroup.test_utils import display_servers
+from usergroup import event_lists
 
 class UserGroupTestSuiteRunner(fix_suite.TestSuiteRunner):
     """A test suite runner which can use a display."""
 
-    def run_tests(self, *args, **kwargs):
+    def run_tests(self, *args, **kw):
         TEST_DISPLAY = os.environ.get('TEST_DISPLAY', '')
+
+        convenient_date = datetime.datetime(2012, 03, 20)
+        def convenient_date_func(**kw):
+            return convenient_date.replace(**kw)
+
+        event_lists.get_date = convenient_date_func
+
         if TEST_DISPLAY:
             if TEST_DISPLAY in ('1', 'True'):
                 server = display_servers.RealDisplay()
             else:
                 server = display_servers.VNCDisplay()
             with server:
-                return fix_suite.TestSuiteRunner.run_tests(self, *args, **kwargs)
+                return fix_suite.TestSuiteRunner.run_tests(self, *args, **kw)
         else:
             #No display needed, just use the normal runner
             return django.test.simple.DjangoTestSuiteRunner.run_tests(self,
-                    *args, **kwargs)
+                    *args, **kw)
 
 suite = fix_suite.create_suite(os.path.dirname(__file__), 'usergroup')
